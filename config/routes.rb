4b51d40c1b1c0f6e68e13fe27ee5540bd22431ff
azuki7898 <hina.apoo.0124@gmail.com>
2021-10-18 13:18:33 +0000

    [Add]update機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get '/' => 'homes#top'
- get 'books' => 'books#index',as:'book'
+ get 'books' => 'books#index',as: 'book'
  post 'books' => 'books#create'
- get 'books/:id' => 'books#show',as:'show_book'
- get 'books/:id/edit' => 'books#edit',as:'edit_book'
- patch 'books/:id' => 'books#update',as:'update_book'
+ get 'books/:id' => 'books#show',as: 'show_book'
+ get 'books/:id/edit' => 'books#edit',as: 'edit_book'
+ patch 'books/:id' => 'books#update',as: 'update_book'
 end
