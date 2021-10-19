0ec5f2c260907fd90392ba0b0c715dbf28a899d8
azuki7898 <hina.apoo.0124@gmail.com>
2021-10-19 04:43:37 +0000

    [Add]削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to show_book_path(book.id)
   end
 
   def show
@@ -22,6 +22,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to show_book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path
   end
 
   private
